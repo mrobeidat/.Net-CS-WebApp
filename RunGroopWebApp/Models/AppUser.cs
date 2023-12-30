2447411fad1228230f309d2075a80a86f0566de3
NTC <NTC@YOUSEF>
2023-12-30 15:58:15 +0300

    Add project files.

---
+++ RunGroopWebApp/Models/AppUser.cs
@@ -0,0 +1,11 @@
+﻿namespace RunGroopWebApp.Models
+{
+    public class AppUser
+    {
+        public int? Pace { get; set; }
+
+        public int? Miklage { get; set; }
+        public Address? Address { get; set; }
+    }
+
+}
