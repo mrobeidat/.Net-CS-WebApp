ffc491f876c596c3d15e0856413192495f2d49b2
mrobeidat <y.linux96@gmail.com>
2023-12-30 19:12:00 +0300

    DB context + entity framework

--- RunGroopWebApp/Models/AppUser.cs
+++ RunGroopWebApp/Models/AppUser.cs
@@ -1,11 +1,19 @@
-﻿namespace RunGroopWebApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RunGroopWebApp.Models
 {
     public class AppUser
     {
+        [Key]
+        public string Id { get; set; }
         public int? Pace { get; set; }
 
         public int? Miklage { get; set; }
         public Address? Address { get; set; }
+
+        public ICollection<Club> Clubs { get; set; }
+
+        public ICollection<Race> Races { get; set; }
     }
 
 }
