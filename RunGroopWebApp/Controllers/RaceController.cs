2ca6e6156f2a283ed1bec442c522fde8c6783247
mrobeidat <y.linux96@gmail.com>
2024-01-04 15:15:24 +0300

    MVC , Context

---
+++ RunGroopWebApp/Controllers/RaceController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RunGroopWebApp.Data;
+
+namespace RunGroopWebApp.Controllers
+{
+    public class RaceController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public RaceController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var Races = _context.Races.ToList();
+            return View(Races);
+        }
+    }
+}
